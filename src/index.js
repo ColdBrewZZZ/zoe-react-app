3b2c9c136bdf446f04c87f236433184137339a4a
zoe <z.g.bradford@gmail.com>
2023-06-13 20:19:37 -0500

    buttons and display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './classComponents/App'; 
+import App from './functionComponents/App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
